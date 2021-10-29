91681140cebd232c83a8a2c444ad7dd19d9253bf
yz <yz@yzdeiMac.local>
2021-10-29 14:44:47 +0800

    我的页面

---
+++ src/screen/tabMineScreen/TabMineScreen.styles.ts
@@ -0,0 +1,126 @@
+import {StyleSheet} from 'react-native'
+import {Size, WINDOW_WIDTH} from "../../tools/WindowTools";
+import {bg_order_num} from "../../file/image/Images";
+
+const styles = StyleSheet.create({
+    container: {
+        width: WINDOW_WIDTH,
+        height: '100%',
+        backgroundColor: '#FAFAFA'
+    },
+    top_box: {
+        width: '100%',
+        paddingHorizontal: Size(16),
+        paddingTop: Size(3),
+        paddingBottom: Size(18),
+        backgroundColor: '#FFFFFF'
+    },
+    user_label_box: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    user_avatar: {
+        width: Size(64),
+        height: Size(64),
+        borderRadius: Size(32),
+        marginLeft: Size(4)
+    },
+    name_and_tips_box: {
+        marginLeft: Size(8)
+    },
+    name: {
+        color: '#303133',
+        fontSize: Size(18)
+    },
+    tips_box: {
+        borderRadius: Size(2),
+        borderColor: '#FF5F93',
+        borderWidth: 1,
+        marginRight: 'auto',
+        marginTop: Size(6)
+    },
+    top_box_text: {
+        color: '#FF5F93',
+        fontSize: Size(10),
+        marginHorizontal: Size(4),
+        marginVertical: Size(2.5)
+    },
+    type_btn: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 'auto'
+    },
+    type_btn_text: {
+        color: '#303133',
+        fontSize: Size(14)
+    },
+    type_btn_icon: {
+        width: Size(14.5),
+        height: Size(14.5),
+        marginLeft: Size(7.5),
+        tintColor: '#606166',
+        transform: [{rotateZ: '90deg'}]
+    },
+    order_and_wallet_box: {
+        marginTop: Size(16.5),
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    order_num_box: {
+        width: Size(173.5),
+        height: Size(75),
+        justifyContent: 'center',
+        paddingLeft: Size(21.5),
+    },
+    order_num_title: {
+        color: '#303133',
+        fontSize: Size(16)
+    },
+    order_num: {
+        color: '#909199',
+        fontSize: Size(11)
+    },
+    bg_wallet_box: {
+        width: Size(173.5),
+        height: Size(75),
+        justifyContent: 'center',
+        paddingLeft: Size(21.5),
+    },
+    bg_wallet_title: {
+        color: '#303133',
+        fontSize: Size(16)
+    },
+    bg_wallet: {
+        color: '#909199',
+        fontSize: Size(11)
+    },
+    bottom_box: {
+        width: '100%',
+        backgroundColor: '#FFFFFF',
+        marginTop: Size(10)
+    },
+    item_box: {
+        width: '100%',
+        height: Size(52),
+        paddingLeft: Size(16),
+        paddingRight: Size(13),
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    item_icon: {
+        width: Size(16),
+        height: Size(16),
+    },
+    item_text: {
+        color: '#303133',
+        fontSize: Size(15),
+        marginLeft: Size(16)
+    },
+    item_icon_arrow: {
+        width: Size(14.5),
+        height: Size(14.5),
+        marginLeft: 'auto'
+    },
+});
+
+export default styles;
--- src/screen/tabMineScreen/TabMineScreen.tsx
+++ src/screen/tabMineScreen/TabMineScreen.tsx
@@ -1,10 +1,69 @@
 import React from "react";
-import {View} from "react-native";
+import {Image, ImageBackground, ImageSourcePropType, Text, TouchableOpacity, View} from "react-native";
+import styles from "./TabMineScreen.styles";
+import {
+    bg_order_num,
+    bg_wallet,
+    icon_agreement,
+    icon_bulb,
+    icon_flag, icon_frown,
+    icon_manual,
+    icon_right_arrow
+} from "../../file/image/Images";
 
 function TabMineScreen() {
+
+    const renderItem = (icon: ImageSourcePropType, text: string) => {
+        return (
+            <View style={styles.item_box}>
+                <Image source={icon} style={styles.item_icon} resizeMode={'contain'}/>
+                <Text style={styles.item_text}>{text}</Text>
+                <Image source={icon_right_arrow} style={styles.item_icon_arrow}/>
+            </View>
+        )
+    }
+
     return (
-        <View style={{width: 100, height: 100, backgroundColor: 'green'}}/>
+        <View style={styles.container}>
+            <View style={styles.top_box}>
+                <View style={styles.user_label_box}>
+                    <Image
+                        style={styles.user_avatar}
+                        source={{uri: 'https://img2.baidu.com/it/u=1129741042,2488904607&fm=26&fmt=auto'}}/>
+                    <View style={styles.name_and_tips_box}>
+                        <Text style={styles.name}>19120821423</Text>
+                        <View style={styles.tips_box}>
+                            <Text style={styles.top_box_text}>初级维客</Text>
+                        </View>
+                    </View>
+                    <TouchableOpacity
+                        style={styles.type_btn}
+                        activeOpacity={0.5}>
+                        <Text style={styles.type_btn_text}>营业</Text>
+                        <Image source={icon_right_arrow} style={styles.type_btn_icon} resizeMode={'contain'}/>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.order_and_wallet_box}>
+                    <ImageBackground source={bg_order_num} resizeMode={'contain'} style={styles.order_num_box}>
+                        <Text style={styles.order_num_title}>接单总数</Text>
+                        <Text style={styles.order_num}>268单</Text>
+                    </ImageBackground>
+                    <ImageBackground source={bg_wallet} resizeMode={'contain'} style={styles.bg_wallet_box}>
+                        <Text style={styles.bg_wallet_title}>我的钱包</Text>
+                        <Text style={styles.bg_wallet}>￥ 123234</Text>
+                    </ImageBackground>
+                </View>
+            </View>
+            <View style={styles.bottom_box}>
+                {renderItem(icon_bulb, '我要服务')}
+                {renderItem(icon_flag, '我要服务')}
+                {renderItem(icon_manual, '使用手册')}
+                {renderItem(icon_agreement, '用户协议')}
+                {renderItem(icon_frown, '意见反馈')}
+            </View>
+        </View>
     )
 }
+
 
 export default TabMineScreen;
